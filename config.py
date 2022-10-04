93551ef0e0a70bacfe71bc5b9c242b6b926b2098
Teslenko Aleksandr <alteslo31@gmail.com>
2022-10-04 11:25:16 +0300

    add migrations

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') \
         or 'b6099382f118f397d6108665e919bba4f90b1614'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
