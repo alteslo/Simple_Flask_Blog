7e843613aec07df4a7bc4b95e9a8dd99371cae27
Teslenko Aleksandr <alteslo31@gmail.com>
2022-12-06 20:13:53 +0300

    add language detect

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,17 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, g
-from flask_login import login_user, logout_user, current_user, login_required
-from werkzeug.urls import url_parse
+
+from flask import flash, g, redirect, render_template, request, url_for
 from flask_babel import _, get_locale
+from flask_login import current_user, login_required, login_user, logout_user
+from langdetect import LangDetectException, detect
+from werkzeug.urls import url_parse
+
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
-    EmptyForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
-from app.models import User, Post
 from app.email import send_password_reset_email
+from app.forms import (EditProfileForm, EmptyForm, LoginForm, PostForm,
+                       RegistrationForm, ResetPasswordForm,
+                       ResetPasswordRequestForm)
+from app.models import Post, User
 
 
 @app.before_request
@@ -24,7 +28,12 @@ def before_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash(_('Your post is now live!'))
